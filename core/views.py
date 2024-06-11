1a9bea1c250c8ce39e05145654b75bd55dd4f5ce
Benjamin <benj.nuneza@duocuc.cl>
2024-06-10 23:07:48 -0400

    agregado de casi todo los html

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-# metodos que llaman a los html
-def home(request):  # resiven siempre como parametro request
-    return render(request ='core/principal.html')
+def home(request):
+    return render(request,'core/principal.html')
