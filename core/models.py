f1304a3a11928020748b38580f27bc78c47b9bd1
Benjamin <benj.nuneza@duocuc.cl>
2024-06-11 10:41:26 -0400

    version estable de la pagina

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto (models.Model):
+    codigo = models.CharField(max_length= 4, primary_key= True)
+    detalle = models.CharField(max_length= 300)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    oferta = models.BooleanField()
+    imagen = models.CharField(max_length=200) # permite agregar la ruta de la imagen
