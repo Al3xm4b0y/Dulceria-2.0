ac9e02b8725376c419badf572426f0c538838c4a
Benjamin <benj.nuneza@duocuc.cl>
2024-06-11 22:02:32 -0400

    agregado de datos dentro y despalayado en la db

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 # creaciom de ruta hacia el archivo html 'principal.html'
 def home(request):
-    return render(request,'core/principal.html')
+    #le vamos a pasar los productos que estan ingresados en el db.
+    dulces = Producto.objects.all()
+    return render(request,'core/principal.html', {'dulces' : dulces})
 # creaciom de ruta hacia el archivo html 's.nosotros.html'
 def nosotros(request):
     return render(request,'core/s.nosotros.html')
